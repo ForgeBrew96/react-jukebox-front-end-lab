a96a6c5cf9a3f0ddc606cc8908f1d3560941754a
ForgeBrew96 <christianbmendoza96@gmail.com>
2024-11-18 12:19:15 -0500

    More changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,125 +1,119 @@
-// src/App.jsx
-import { useState, useEffect } from 'react'
-import * as trackService from './services/trackService'
+import { useState, useEffect } from 'react';
+import * as trackService from './services/trackService';
 
-import Home from "./components/Home"
-import TrackList from "./components/TrackList"
-import TrackDetail from "./components/TrackDetail"
-import TrackForm from "./components/TrackForm"
+import Home from "./components/Home";
+import TrackList from "./components/TrackList";
+import TrackDetail from "./components/TrackDetail";
+import TrackForm from "./components/TrackForm";
 
 const App = () => {
-const [trackList, setTrackList] = useState([])
-const [selected, setSelected] = useState(null)
-const [isFormOpen, setIsFormOpen] = useState(false)
+  const [trackList, setTrackList] = useState([]);
+  const [selected, setSelected] = useState(null);
+  const [isFormOpen, setIsFormOpen] = useState(false);
 
-useEffect(() => {
- const fetchTracks = async () => {
+  useEffect(() => {
+    const fetchTracks = async () => {
+      try {
+        const tracks = await trackService.index();
 
-try {
-  const tracks = await trackService.index();
+        if (tracks.error) {
+          throw new Error(tracks.error);
+        }
 
-  if (tracks.error) {
-    throw new Error(tracks.error);
-  }
- 
-  setTrackList(tracks)
-} catch (error) {
-  console.log(error)
-}
- };
- fetchTracks();
-}, []);
+        setTrackList(tracks);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchTracks();
+  }, []);
 
-const updateSelected = (track) => {
-  setSelected(track)
-}
+  const updateSelected = (track) => {
+    setSelected(track);
+  };
 
-const handleFormView = (track) => {
-  if (!track.name) setSelected(null);
-  setIsFormOpen(!isFormOpen)
-}
+  const handleFormView = (isOpen, track = null) => {
+    setSelected(track);
+    setIsFormOpen(isOpen);
+  };
 
-const handleAddTrack = async (formData) => {
-  try {
-    const newTrack = await trackService.create(formData);
+  const handleAddTrack = async (formData) => {
+    try {
+      const newTrack = await trackService.create(formData);
 
-    if (newTrack.error) {
-      throw new Error(newTrack.error)
+      if (newTrack.error) {
+        throw new Error(newTrack.error);
+      }
+
+      setTrackList([newTrack, ...trackList]);
+      setIsFormOpen(false);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    setTrackList([newTrack, ...trackList])
-    setIsFormOpen(false)
-  } catch (error) {
-    console.log(error);
-  }
-}
+  const handleUpdateTrack = async (formData, trackId) => {
+    try {
+      const updatedTrack = await trackService.updateTrack(formData, trackId);
 
-const handleUpdateTrack = async (formData, trackId) => {
-  try {
-    const updatedTrack = await trackService.updateTrack(formData, trackId);
+      if (updatedTrack.error) {
+        throw new Error(updatedTrack.error);
+      }
 
-    if (updatedTrack.error) {
-      throw new Error(updatedTrack.error)
+      const updatedTrackList = trackList.map((track) =>
+        track._id !== updatedTrack._id ? track : updatedTrack
+      );
+
+      setTrackList(updatedTrackList);
+      setSelected(updatedTrack);
+      setIsFormOpen(false);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const updatedTrackList = trackList.map((track) =>
-      track._id !== updatedTrack._id ? track : updatedTrack
-    );
+  const handleRemoveTrack = async (trackId) => {
+    try {
+      const deleteResponse = await trackService.deleteTrack(trackId);
+      if (deleteResponse && deleteResponse.error) {
+        throw new Error(deleteResponse.error);
+      }
 
-    setTrackList(updatedTrackList)
-    setSelected(updatedTrack);
-    setIsFormOpen(false)
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-const handleRemoveTrack = async (track) => {
-  try {
-    const trackId = track._id; 
-    const deleteResponse = await trackService.deleteTrack(trackId);
-    if (deleteResponse && deleteResponse.error) {
-      throw new Error(deleteResponse.error);
+      setTrackList((prevTrackList) => {
+        const updatedTrackList = prevTrackList.filter((t) => t._id !== trackId);
+        return updatedTrackList;
+      });
+      setSelected(null); 
+      setIsFormOpen(false);
+    } catch (error) {
+      console.log(error);
     }
-
-    setTrackList((prevTrackList) => {
-      const updatedTrackList = prevTrackList.filter((t) => t._id !== trackId);
-      return updatedTrackList;
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-
+  };
 
   return (
     <>
       <Home />
       <TrackList 
-      selected={selected}
-      trackList={trackList}
-      updatedSelected={updateSelected}
-      handleFormView={handleFormView}
-      handleRemoveTrack={handleRemoveTrack}
-      handleUpdateTrack={handleUpdateTrack}
-      isFormOpen={isFormOpen}
+        selected={selected}
+        trackList={trackList}
+        updateSelected={updateSelected}
+        handleFormView={handleFormView}
+        handleRemoveTrack={handleRemoveTrack}
+        isFormOpen={isFormOpen}
       />
       {isFormOpen ? (
-        <TrackForm handleAddTrack={handleAddTrack}
-        handleUpdateTrack={handleUpdateTrack}
-        selected={selected}
+        <TrackForm 
+          handleAddTrack={handleAddTrack}
+          handleUpdateTrack={handleUpdateTrack}
+          selected={selected}
         />
       ) : (
         <TrackDetail
-        selected={selected}
-        handleFormView={handleFormView}
-        handleRemoveTrack={handleRemoveTrack}
+          selected={selected}
         />
-      )
-    }
+      )}
     </>
-  )
+  );
 };
 
 export default App;
--- src/components/TrackList.jsx
+++ src/components/TrackList.jsx
@@ -1,24 +1,37 @@
 const TrackList = (props) => {
+    const handleEditTrack = (track) => {
+      props.updateSelected(track);
+      props.handleFormView(true, track);
+    };
+  
+    const handleDeleteTrack = (trackId) => {
+      props.updateSelected(null);
+      props.handleRemoveTrack(trackId);
+    };
+  
+    const handlePlayTrack = (track) => {
+      props.updateSelected(track);
+    };
+  
     const tracks = props.trackList.map((track) =>
-        <li key={track._id} onClick={ () => props.updatedSelected(track)}>
-            <a href="#">{track.title}</a>
-
-            <button onClick={''}>Play</button>
-            <button onClick={() => props.handleFormView(props.selected)}>Edit</button>
-        <button onClick={() => props.handleRemoveTrack(props.selected)}>Delete</button>
-        </li>
+      <li key={track._id}>
+        <a href="#" onClick={() => handlePlayTrack(track)}>{track.title}</a>
+        <button onClick={() => handlePlayTrack(track)}>Play</button>
+        <button onClick={() => handleEditTrack(track)}>Edit</button>
+        <button onClick={() => handleDeleteTrack(track._id)}>Delete</button>
+      </li>
     );
-
-    return(
-        <div>
-          <h1>Track List</h1>
-          {!props.trackList.length ? <h2>No Tracks Yet!</h2> : <ul>{tracks}</ul>}
-
-          <button onClick={props.handleFormView}>
-              {props.isFormOpen ? 'Close Form' : 'New Track'}
-          </button>
+  
+    return (
+      <div>
+        <h1>Track List</h1>
+        {!props.trackList.length ? <h2>No Tracks Yet!</h2> : <ul>{tracks}</ul>}
+        <button onClick={() => props.handleFormView(true)}>
+          {props.isFormOpen ? 'Close Form' : 'New Track'}
+        </button>
       </div>
-    )
-}
-
-export default TrackList
+    );
+  };
+  
+  export default TrackList;
+  
